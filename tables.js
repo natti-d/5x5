1fc55281d9f3034ee44b2eca247b7cb9b18b2b7d
jocosep121 <jocosep578@ikanid.com>
2023-12-20 19:28:42 +0200

    \n

--- tables.js
+++ tables.js
@@ -3,7 +3,7 @@ var products = [
         "id": 0,
         "name": "Brazil Nuts",
         "brand": "Smart Organic",
-        "info": "Have you tried brazil nuts? They are:/n– The food with the highest selenium content/n– Rich in healthy fats/n– Rich in antioxidants/n– Raw/n– Organic/n/nBrazil nuts are nutritional powerhouses, providing healthy fats, antioxidants, vitamins, and minerals. They’re particularly high in selenium, a mineral with potent antioxidant properties. Eating Brazil nuts may reduce inflammation, support brain function, and improve your thyroid function and heart health./nTo avoid consuming too much selenium, limit your intake to one to three Brazil nuts per day./nRecommended use: Add Brazil nuts to your breakfast or consume as a snack./nIngredients: Brazil nuts*./n*Organic certified ingredient.",
+        "info": "Have you tried brazil nuts? They are:\n -The food with the highest selenium content\n– Rich in healthy fats\n– Rich in antioxidants\n– Raw\n– Organic\n\nBrazil nuts are nutritional powerhouses, providing healthy fats, antioxidants, vitamins, and minerals. They’re particularly high in selenium, a mineral with potent antioxidant properties. Eating Brazil nuts may reduce inflammation, support brain function, and improve your thyroid function and heart health.\nTo avoid consuming too much selenium, limit your intake to one to three Brazil nuts per day.\nRecommended use: Add Brazil nuts to your breakfast or consume as a snack.\nIngredients: Brazil nuts*.\n*Organic certified ingredient.",
         "image": "./image/brazilnuts.png",
         "price": "13.67",
         "quantity": "150 g"
@@ -12,7 +12,7 @@ var products = [
         "id": 1,
         "name": "Vegetable Chips",
         "brand": "Snack Day",
-        "info": "BIO Organic vegetable chips with sea salt, made from parsnip, carrot, sweet potatoes, beetroot, palm oil free./nIngredients:/n62% vegetables in variable proportions (parsnip*, carrot*, sweet potatoes*, beetroot*), 37% sunflower oil*, 1% sea salt - *from organic farming",
+        "info": "BIO Organic vegetable chips with sea salt, made from parsnip, carrot, sweet potatoes, beetroot, palm oil free.\nIngredients:\n62% vegetables in variable proportions (parsnip*, carrot*, sweet potatoes*, beetroot*), 37% sunflower oil*, 1% sea salt - *from organic farming",
         "image": "./image/chips.png",
         "price": "7.80",
         "quantity": "120 g"
@@ -21,7 +21,7 @@ var products = [
         "id": 2,
         "name": "Chocolate Balls",
         "brand": "Turtle",
-        "info": "Elevate your breakfast experience with Turtle Choco Balls, the perfect blend of wholesome cereals and real, organic chocolate./n/nDid you know that our cocoa is sourced from the hot and humid jungles, where the cocoa tree flourishes? The beans from these pods are carefully dried and ground to create the rich cocoa powder that makes our Choco Balls an indulgent morning treat. Start your mornings right with the goodness of Turtle Choco Balls!/n/nIngredients: cereal flour* 69,1% (rice*, corn*, WHEAT*), cane sugar*, couverture MILK chocolate* 4,7% (cane sugar*, MILK powder*, cocoa butter*, cocoa mass*, emulsifier: SOY lecithin*, vanilla extract*), corn syrup*, fat reduced cocoa powder* 2.9%, cocoa powder* 1%, emulsifier: SOY lecithin*, salt. *Certified organic ingredients from EU/non-EU agriculture.",
+        "info": "Elevate your breakfast experience with Turtle Choco Balls, the perfect blend of wholesome cereals and real, organic chocolate.\n\nDid you know that our cocoa is sourced from the hot and humid jungles, where the cocoa tree flourishes? The beans from these pods are carefully dried and ground to create the rich cocoa powder that makes our Choco Balls an indulgent morning treat. Start your mornings right with the goodness of Turtle Choco Balls!\n\nIngredients: cereal flour* 69,1% (rice*, corn*, WHEAT*), cane sugar*, couverture MILK chocolate* 4,7% (cane sugar*, MILK powder*, cocoa butter*, cocoa mass*, emulsifier: SOY lecithin*, vanilla extract*), corn syrup*, fat reduced cocoa powder* 2.9%, cocoa powder* 1%, emulsifier: SOY lecithin*, salt. *Certified organic ingredients from EU\non-EU agriculture.",
         "image": "./image/chocolateballs.png",
         "price": "6.94",
         "quantity": "300 g"
@@ -30,7 +30,7 @@ var products = [
         "id": 3,
         "name": "Mini Choco Chips",
         "brand": "Gullón",
-        "info": "Mini Choco Chips are cookies with chocolate chips in a small and delicious format./n/nIngredients: Wheat flour , dark chocolate chips (sugar, cocoa mass, cocoa butter, emulsifier ( soy lecithin ), vegetable oil (high oleic sunflower), sugar, glucose and fructose syrup, salt, raising agents (acid carbonate sodium and ammonium hydrogen carbonate), aroma. May contain traces of Nuts and Milk.",
+        "info": "Mini Choco Chips are cookies with chocolate chips in a small and delicious format.\n\nIngredients: Wheat flour , dark chocolate chips (sugar, cocoa mass, cocoa butter, emulsifier ( soy lecithin ), vegetable oil (high oleic sunflower), sugar, glucose and fructose syrup, salt, raising agents (acid carbonate sodium and ammonium hydrogen carbonate), aroma. May contain traces of Nuts and Milk.",
         "image": "./image/chocolatecookie.png",
         "price": "2.25",
         "quantity": "150 g"
@@ -39,7 +39,7 @@ var products = [
         "id": 4,
         "name": "Goji Berries",
         "brand": "Dragon Superfoods",
-        "info": "Dragon Superfoods Goji Berries contain whole dried fruits that are rich in amino acids, vitamins and minerals. The fruits are orange-red in color and have a naturally sweet-sour taste. The antioxidants carotene and zeaxanthin in goji berries protect the retina and improve vision. Dried fruits have a beneficial effect on the kidneys and regenerate cells./n/nMethod of use:/nAdd the product to smoothies or sprinkle it on desserts, muesli or yogurt./n/nComposition:/nGoji berry (100%)*./n*organic certified product.",
+        "info": "Dragon Superfoods Goji Berries contain whole dried fruits that are rich in amino acids, vitamins and minerals. The fruits are orange-red in color and have a naturally sweet-sour taste. The antioxidants carotene and zeaxanthin in goji berries protect the retina and improve vision. Dried fruits have a beneficial effect on the kidneys and regenerate cells.\n\nMethod of use:\nAdd the product to smoothies or sprinkle it on desserts, muesli or yogurt.\n\nComposition:\nGoji berry (100%)*.\n*organic certified product.",
         "image": "./image/goji.jpg",
         "price": "6.99",
         "quantity": "100 g"
@@ -66,7 +66,7 @@ var products = [
         "id": 7,
         "name": "Carrot and apple juice",
         "brand": "Zdravo",
-        "info": "Single Item /nJuice in 1 packaging /nVegan product /nNo added water and sugar /nNo artificial preservatives, colors, flavors, or sweeteners",
+        "info": "Single Item \nJuice in 1 packaging \nVegan product \nNo added water and sugar \nNo artificial preservatives, colors, flavors, or sweeteners",
         "image": "./image/juicecarrot.png",
         "price": "11.65",
         "quantity": "750 ml"
@@ -84,7 +84,7 @@ var products = [
         "id": 9,
         "name": "Kokosolie",
         "brand": "Mattison",
-        "info": " Pure coconut oil /nCold-pressed, unrefined, organic /nVersatile in use: baking, roasting, frying and in smoothies /nAlso for skin care",
+        "info": " Pure coconut oil \nCold-pressed, unrefined, organic |Versatile in use: baking, roasting, frying and in smoothies \nAlso for skin care",
         "image": "./image/kokosowomaslo.png",
         "price": "37.04",
         "quantity": "500 ml"
