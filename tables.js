6774fe2d854da3ebc80882d70e3e31f60cf9af24
nati <11>
2023-12-20 17:57:44 +0200

    update

---
+++ tables.js
@@ -0,0 +1,86 @@
+var products = [
+    {
+        "id": 0,
+        "name": "Brazil Nuts",
+        "brand": "Smart Organic",
+        "info": "",
+        "image": "./image/brazilnuts.png",
+        "price": "13.67",
+        "quantity": "150 g"
+    },
+    {
+        "id": 1,
+        "name": "Vegetable Chips",
+        "brand": "Snack Day",
+        "info": "",
+        "image": "./image/chips.png",
+        "price": "7.80",
+        "quantity": "120 g"
+    },
+    {
+        "id": 2,
+        "name": "Chocolate Balls",
+        "brand": "Turtle",
+        "info": "",
+        "image": "./image/chocolateballs.png",
+        "price": "6.94",
+        "quantity": "300 g"
+    },
+    {
+        "id": 3,
+        "name": "Mini Choco Chips",
+        "brand": "Gullón",
+        "info": "",
+        "image": "./image/chocolatecookie.png",
+        "price": "2.25",
+        "quantity": "150 g"
+    },
+    {
+        "id": 4,
+        "name": "Goji Berries",
+        "brand": "Aldi",
+        "info": "",
+        "image": "./image/goji.png",
+        "price": "44.69",
+        "quantity": "1 kg"
+    },
+    {
+        "id": 5,
+        "name": "Millet balls",
+        "brand": "Biopont",
+        "info": "",
+        "image": "./image/milletballs.png",
+        "price": "2.27",
+        "quantity": "60 g"
+    },
+    {
+        "id": 6,
+        "name": "Christmas Tea",
+        "brand": "Yogi Organic",
+        "info": "",
+        "image": "./image/tea.png",
+        "price": "4.99",
+        "quantity": "60 g"
+    },
+    {
+        "id": 6,
+        "name": "Christmas Tea",
+        "brand": "Yogi Organic",
+        "info": "",
+        "image": "./image/tea.png",
+        "price": "4.99",
+        "quantity": "60 g"
+    },
+
+    /*Шаблон
+    {
+        "id": 0,
+        "name": "",
+        "brand": "",
+        "info": "" ,
+        "image": "./image/",
+        "price": "",
+        "quantity": ""
+    }
+     */
+];
